f4df09c432b43a39939042c38a325abb720cb7b4
David Fialho <fialho.david@protonmail.com>
2018-02-13 11:12:49 +0000

    Have radicale log to a file

--- files/bin/wrapper.sh
+++ files/bin/wrapper.sh
@@ -6,3 +6,4 @@ radicale --config $CONFIG_FILE 					\
 	--storage-filesystem-folder=$STORAGE_DIR 	\
 	--auth-htpasswd-filename=$USERS_FILE 		\
 	--server-pid=$PID_FILE		 				\
+	--logging-config=$LOGGERS_FILE 				\
