38fc16debcc495ba24b138386c0cb40636fbd3b3
David Fialho <fialho.david@protonmail.com>
2018-02-13 12:39:49 +0000

    Create git repository in wrapper
    This avoids potential race conditions where multiple users
    make initial changes at the same time and they both try to
    create a repository.

--- files/bin/commit.sh
+++ files/bin/commit.sh
@@ -4,25 +4,6 @@ set -e
 . ${SNAP}/defs.sh
 
 cd $STORAGE_DIR
-
-# Create git repository to store versions of the user's data
-if [ ! -d .git/ ]; then
-	# This is a bogus email and name
-	git config --global user.email "radicale@mail.com"
-	git config --global user.name "radicale"
-
-	git init
-
-	# include a gitignore with the radicale tmp files
-	echo ".Radicale.cache" > .gitignore
-	echo ".Radicale.lock" >> .gitignore
-	echo ".Radicale.tmp.*" >> .gitignore
-	echo ".Radicale.props" >> .gitignore
-
-	git add .gitignore
-	git commit -m "Initial commit"
-fi
-
 git add -A
 git diff --cached --quiet || git commit -m "Changes by $1 at $(date)"
 
--- files/bin/wrapper.sh
+++ files/bin/wrapper.sh
@@ -4,6 +4,26 @@ set -e
 . $SNAP/defs.sh
 
 
+# Create git repository to store versions of the user's data
+cd $STORAGE_DIR
+if [ ! -d .git/ ]; then
+	# This is a bogus email and name
+	git config --global user.email "radicale@mail.com"
+	git config --global user.name "radicale"
+
+	git init
+
+	# include a gitignore with the radicale tmp files
+	echo ".Radicale.cache" > .gitignore
+	echo ".Radicale.lock" >> .gitignore
+	echo ".Radicale.tmp.*" >> .gitignore
+	echo ".Radicale.props" >> .gitignore
+
+	git add .gitignore
+	git commit -m "Initial commit"
+fi
+
+
 radicale --config $CONFIG_FILE 					\
 	--storage-filesystem-folder=$STORAGE_DIR 	\
 	--auth-htpasswd-filename=$USERS_FILE 		\
