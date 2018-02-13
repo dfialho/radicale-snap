077441b025f26d2f6a39c9da18dec523362ad381
David Fialho <fialho.david@protonmail.com>
2018-02-13 10:17:05 +0000

    Daemonize radicale app

--- files/bin/wrapper.sh
+++ files/bin/wrapper.sh
@@ -2,6 +2,7 @@
 
 . $SNAP/defs.sh
 
-radicale --config $CONFIG_FILE \
-	--storage-filesystem-folder=$STORAGE_DIR \
-	--auth-htpasswd-filename=$USERS_FILE \
+radicale --config $CONFIG_FILE 					\
+	--storage-filesystem-folder=$STORAGE_DIR 	\
+	--auth-htpasswd-filename=$USERS_FILE 		\
+	--server-pid=$PID_FILE		 				\
