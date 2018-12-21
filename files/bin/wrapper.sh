a6049d99a434f60b4e95ee28ee43e4c2668b8671
David Fialho <dfialho@protonmail.com>
2018-12-21 13:35:46 +0000

    Use simple daemon instead of forking

--- files/bin/wrapper.sh
+++ files/bin/wrapper.sh
@@ -28,7 +28,6 @@ function radicale {
 	${SNAP}/bin/radicale --config ${CONFIG_FILE} 	\
 		--storage-filesystem-folder=${STORAGE_DIR} 	\
 		--auth-htpasswd-filename=${USERS_FILE} 		\
-		--server-pid=${PID_FILE}		 			\
 		--logging-config=${LOGGERS_FILE} 			\
 		--server-hosts=0.0.0.0:$(snapctl get port) $@
 }
